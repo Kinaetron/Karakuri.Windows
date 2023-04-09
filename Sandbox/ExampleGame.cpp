c07cf25e98de7f8534f47f3589256ca8c26f2b27
Darren Daley <darren.daley@hotmail.co.uk>
2023-04-09 14:00:55 +0100

    using global namespaces

--- Sandbox/ExampleGame.cpp
+++ Sandbox/ExampleGame.cpp
@@ -2,13 +2,15 @@
 #include "Graphics/Colors.h"
 #include "Core/EntryPoint.h"
 
+using namespace Karakuri;
+using namespace DirectX::SimpleMath;
 
 void ExampleGame::Initalize()
 {
-	test1 = Karakuri::Texture(Graphics, L"test.jpg");
-	test2 = Karakuri::Texture(Graphics, L"test2.jpg");
-	test3 = Karakuri::Texture(Graphics, L"test3.jpg");
-	renderer = Karakuri::SpriteRenderer(Graphics);
+	test1 = Texture(Graphics, L"test.jpg");
+	test2 = Texture(Graphics, L"test2.jpg");
+	test3 = Texture(Graphics, L"test3.jpg");
+	renderer = SpriteRenderer(Graphics);
 }
 
 void ExampleGame::Update()
@@ -17,11 +19,11 @@ void ExampleGame::Update()
 
 void ExampleGame::Draw()
 {
-	Graphics->Clear(Karakuri::Colors::CornflowerBlue);
+	Graphics->Clear(Colors::Beige);
 
-	//renderer.Draw(test1, DirectX::SimpleMath::Vector2(100.0f, 100.0f), DirectX::SimpleMath::Vector2(828.0f, 809.0f), 0.0f, Karakuri::Colors::White);
-	//renderer.Draw(test2, DirectX::SimpleMath::Vector2(1300.0f, 400.0f), DirectX::SimpleMath::Vector2(555.0f, 558.0f), 0.0f, Karakuri::Colors::White);
-	renderer.Draw(test3, DirectX::SimpleMath::Vector2(0.0f, 0.0f), DirectX::SimpleMath::Vector2(1080.0f, 778.0f), DirectX::SimpleMath::Vector2::Zero, 0.0f, Karakuri::Colors::White);
+	renderer.Draw(test2, Vector2(1300.0f, 400.0f), DirectX::SimpleMath::Vector2(555.0f, 558.0f), Vector2::Zero, 0.0f, Colors::White);
+	renderer.Draw(test3, Vector2(0.0f, 0.0f), DirectX::SimpleMath::Vector2(1080.0f, 778.0f), Vector2::Zero, 0.0f, Colors::White);
+	renderer.Draw(test1, Vector2(100.0f, 100.0f), DirectX::SimpleMath::Vector2(828.0f, 809.0f), Vector2::Zero, 0.0f,Colors::White);
 
 	Graphics->Present();
 }
